75b4fe1493d6f81f13b7ff4faeb0ef9dca814415
unknown <lso@.RGU.local>
2009-11-03 14:25:18 +0100

    Initialized simple script

---
+++ src/grawler.groovy
@@ -0,0 +1,15 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: lso
+ * Date: 03.11.2009
+ * Time: 12:52:29
+ * To change this template use File | Settings | File Templates.
+ */
+
+def searchFor = /aWYoIWl/
+new File('.').eachFileRecurse {
+  def myMatcher = (it.getText() =~ searchFor)
+  if (myMatcher.getCount()){
+    println "Found in file: {$it}"
+  }
+}
