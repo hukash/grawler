d01c445d0f56c732251b872158338ffb67340b90
Lukas Sobik <hukash@gmail.com>
2009-12-07 23:31:30 +0100

    started python script

---
+++ prawler.py
@@ -0,0 +1,12 @@
+class Prawler:
+    counter = 0
+    results = []
+
+    def search_for_injection(self, pattern, dir, filetype):
+        pass
+
+    def view_results(self):
+        pass
+
+    def delete_infected_chunks(self, pattern):
+        pass
